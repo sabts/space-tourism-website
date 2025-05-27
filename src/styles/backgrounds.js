f43afb90d58e3c9f65fe6ba079a03af4b5fa46dd
sabts <sabrinataraschi@gmail.com>
2025-05-27 16:21:39 +0200

    Create backgrounds.js

---
+++ src/styles/backgrounds.js
@@ -0,0 +1,23 @@
+const home = {
+    mobile: "/assets/images/home/background-home-mobile.jpg",
+    tablet: "/assets/images/home/background-home-tablet.jpg",
+    desktops: "/assets/images/home/background-home-desktop.jpg"
+}
+
+const destination = {
+    mobile: "/assets/images/destination/background-destination-mobile.jpg",
+    tablet: "/assets/images/destination/background-destination-tablet.jpg",
+    desktops: "/assets/images/destination/background-destination-desktop.jpg"
+}
+
+const crew = {
+    mobile: "/assets/images/crew/background-crew-mobile.jpg",
+    tablet: "/assets/images/crew/background-crew-tablet.jpg",
+    desktops: "/assets/images/crew/background-crew-desktop.jpg"
+}
+
+const technology = {
+    mobile: "/assets/images/technology/background-technology-mobile.jpg ",
+    tablet: "/assets/images/technology/background-technology-tablet.jpg ",
+    desktops: "/assets/images/technology/background-technology-desktop.jpg "
+}
